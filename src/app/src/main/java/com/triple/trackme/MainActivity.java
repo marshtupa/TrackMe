9555febc2462eb2f019e4a87838a40b0e3f9e1f8
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-07-10 22:20:50 +0300

    Repair last merge

--- src/app/src/main/java/com/triple/trackme/MainActivity.java
+++ src/app/src/main/java/com/triple/trackme/MainActivity.java
@@ -27,39 +27,12 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 
-//Библиотеки для работы с файлами
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-
-//добавить всплывающее сообщение
-import android.widget.Toast;
-
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, OnMapLoadedCallback, LocationListener {
 
     private GoogleMap map;
     private ProgressDialog progressDialog;
-////////////////////////////////Константы////////////////////////////////////////////////////
-    final String LOG_TAG = "myLogs";
-    final String FILENAME = "file";
 
-    final String DIR_SD = "MyFiles";
-    final String FILENAME_SD = "fileSD";
-    //////////////////////////////////////////////////////////////////////////////////////////////
     public static final int REQUEST_ID_ACCESS_COURSE_FINE_LOCATION = 100;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -161,126 +134,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
         return bestProvider;
     }
-  
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    ////////////////////////////////Функции кнопок////////////////////////////////////////////////////
-
-    public void onClick1(View v){
-        String filestr;
-        writeFile(FILENAME,"Something");
-        filestr = readFile(FILENAME);
-        Toast toast = Toast.makeText(this, filestr,Toast.LENGTH_LONG);
-        toast.show();
-    }
-    public void onClick2(View v){
-
-    }
-    public void onClick3(View v){
-
-    }
-    public void onClick4(View v){
-
-    }
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////Функции сохранения и загруки///////////////////////////////////////
-
-    void writeFile(String filename, String Value) {
-        try {
-            // отрываем поток для записи
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-                    openFileOutput(filename, MODE_PRIVATE)));
-            // пишем данные
-            bw.write(Value);
-            // закрываем поток
-            bw.close();
-            Log.d(LOG_TAG, "Файл записан");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    String readFile(String filename) {
-        String str = "";
-        String str2 = "";
-        try {
-            // открываем поток для чтения
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    openFileInput(filename)));
-            // читаем содержимое
-            while ((str = br.readLine()) != null) {
-                Log.d(LOG_TAG, str);
-                str2 = str2 + str;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return str2;
-    }
-
-    void writeFileSD() {
-        // проверяем доступность SD
-        if (!Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED)) {
-            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
-            return;
-        }
-        // получаем путь к SD
-        File sdPath = Environment.getExternalStorageDirectory();
-        // добавляем свой каталог к пути
-        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
-        // создаем каталог
-        sdPath.mkdirs();
-        // формируем объект File, который содержит путь к файлу
-        File sdFile = new File(sdPath, FILENAME_SD);
-        try {
-            // открываем поток для записи
-            BufferedWriter bw = new BufferedWriter(new FileWriter(sdFile));
-            // пишем данные
-            bw.write("Содержимое файла на SD");
-            // закрываем поток
-            bw.close();
-            Log.d(LOG_TAG, "Файл записан на SD: " + sdFile.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void readFileSD() {
-        // проверяем доступность SD
-        if (!Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED)) {
-            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
-            return;
-        }
-        // получаем путь к SD
-        File sdPath = Environment.getExternalStorageDirectory();
-        // добавляем свой каталог к пути
-        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
-        // формируем объект File, который содержит путь к файлу
-        File sdFile = new File(sdPath, FILENAME_SD);
-        try {
-            // открываем поток для чтения
-            BufferedReader br = new BufferedReader(new FileReader(sdFile));
-            String str = "";
-            // читаем содержимое
-            while ((str = br.readLine()) != null) {
-                Log.d(LOG_TAG, str);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
 
     private void showMyLocation() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -343,6 +196,5 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onProviderDisabled(String provider) { }
-
 
 }
