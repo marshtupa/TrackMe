7d62a34b8361da8425faa9e29ed8837316a7aff5
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-07-11 00:12:39 +0300

    Update MainActivity

--- src/app/src/main/java/com/triple/trackme/MainActivity.java
+++ src/app/src/main/java/com/triple/trackme/MainActivity.java
@@ -2,7 +2,6 @@ package com.triple.trackme;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
@@ -11,9 +10,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-import android.widget.RelativeLayout;
 import android.view.View;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -25,7 +22,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, OnMapLoadedCallback, LocationListener {
 
@@ -33,6 +29,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private ProgressDialog progressDialog;
 
     public static final int REQUEST_ID_ACCESS_COURSE_FINE_LOCATION = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +42,19 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    private void showMapLoadProgress() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Map Loading ...");
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCancelable(true);
+        progressDialog.show();
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         map.setOnMapLoadedCallback(this);
-        settingMap(map);
+        GoogleMapService.settingMap(map, getSupportFragmentManager(), this);
 
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -73,34 +78,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         showMyLocation();
     }
 
-    private void showMapLoadProgress() {
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Map Loading ...");
-        progressDialog.setMessage("Please wait...");
-        progressDialog.setCancelable(true);
-        progressDialog.show();
-    }
-
-    private void settingMap(GoogleMap googleMap) {
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_map));
-
-        map.getUiSettings().setZoomControlsEnabled(true);
-        map.getUiSettings().setCompassEnabled(false);
-        map.getUiSettings().setMyLocationButtonEnabled(true);
-        map.getUiSettings().setZoomControlsEnabled(false);
-
-        final int CURRENT_POSITION_BUTTON_ID = 0x2;
-        Fragment mapFragment = getSupportFragmentManager().findFragmentById(R.id.map);
-        View currentPositionButton = mapFragment.getView().findViewById(CURRENT_POSITION_BUTTON_ID);
-        if (currentPositionButton != null && currentPositionButton.getLayoutParams() instanceof RelativeLayout.LayoutParams)
-        {
-            RelativeLayout.LayoutParams params_zoom = (RelativeLayout.LayoutParams) currentPositionButton.getLayoutParams();
-            params_zoom.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-            params_zoom.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        }
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -110,35 +87,22 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 if (grantResults.length > 1
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED
                         && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    showLogInfo("MapInfo", "Permission granted!");
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                    Log.i("MapInfo", "Permission denied!");
                     this.showMyLocation();
                 }
                 else {
-                    showLogInfo("MapInfo", "Permission denied!");
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                    Log.i("MapInfo", "Permission denied!");
                 }
                 break;
             }
         }
     }
 
-    private String getEnabledLocationProvider() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        Criteria criteria = new Criteria();
-        String bestProvider = locationManager.getBestProvider(criteria, true);
-        boolean enabled = locationManager.isProviderEnabled(bestProvider);
-
-        if (!enabled) {
-            showLogInfo("MapInfo", "No location provider enabled!");
-            return null;
-        }
-        return bestProvider;
-    }
-
     private void showMyLocation() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        String locationProvider = this.getEnabledLocationProvider();
+        String locationProvider = GoogleMapService.getEnabledLocationProvider(locationManager, this);
         if (locationProvider == null) {
             return;
         }
@@ -154,7 +118,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             myLocation = locationManager.getLastKnownLocation(locationProvider);
         }
         catch (SecurityException e) {
-            showLogInfo("MapInfo", "Show My Location Error: " + e.getMessage());
+            Toast.makeText(this, "Show My Location Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.i("MapInfo", "Show My Location Error: " + e.getMessage());
             e.printStackTrace();
             return;
         }
@@ -170,20 +135,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                     .build();
             map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         } else {
-            showLogInfo("MapInfo", "Location not found");
+            Toast.makeText(this, "Location not found", Toast.LENGTH_LONG).show();
+            Log.i("MapInfo", "Location not found");
         }
 
     }
 
-    private void showLogInfo(String tag, String message)
-    {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-        Log.i(tag, message);
-    }
-
-    public void clickProfileButton(View view) {
-        Log.i("buttonClick", "Profile button click!!!");
-    }
 
     @Override
     public void onLocationChanged(Location location) { }
@@ -196,5 +153,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onProviderDisabled(String provider) { }
+
+
+    public void clickProfileButton(View view) {
+        Log.i("buttonClick", "Profile button click!!!");
+    }
 
 }
