a3f307332e1c435c6481e86436b354349284c5d6
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-09-14 12:47:46 +0300

    Set style for image map

--- src/app/src/main/java/com/triple/trackme/Services/ImageMapHelper.java
+++ src/app/src/main/java/com/triple/trackme/Services/ImageMapHelper.java
@@ -21,6 +21,7 @@ public class ImageMapHelper {
         ImageMapHelper.url = new StringBuilder();
         initializeUrl();
         setCenter();
+        setStyle();
         setZoom();
         setSize();
         setPath();
@@ -47,6 +48,105 @@ public class ImageMapHelper {
         else {
             url.append("center=" + centerPosition.latitude + "," + centerPosition.longitude);
         }
+    }
+
+    private static void setStyle() {
+        url.append("&style=" +
+                "element:geometry" + "%7C" +
+                "color:0xebe3cd");
+        url.append("&style=" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0x523735");
+        url.append("&style=" +
+                "element:labels.text.stroke" + "%7C" +
+                "color:0xf5f1e6");
+        url.append("&style=" +
+                "feature:administrative" + "%7C" +
+                "element:geometry.stroke" + "%7C" +
+                "color:0xc9b2a6");
+        url.append("&style=" +
+                "feature:administrative.land_parcel" + "%7C" +
+                "element:geometry.stroke" + "%7C" +
+                "color:0xdcd2be");
+        url.append("&style=" +
+                "feature:administrative.land_parcel" + "%7C" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0xae9e90");
+        url.append("&style=" +
+                "feature:landscape.natural" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xdfd2ae");
+        url.append("&style=" +
+                "feature:poi" + "%7C" +
+                "visibility:off");
+        url.append("&style=" +
+                "feature:poi" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xdfd2ae");
+        url.append("&style=" +
+                "feature:poi" + "%7C" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0x93817c");
+        url.append("&style=" +
+                "feature:poi.park" + "%7C" +
+                "element:geometry.fill" + "%7C" +
+                "color:0xa5b076");
+        url.append("&style=" +
+                "feature:poi.park" + "%7C" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0x447530");
+        url.append("&style=" +
+                "feature:road" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xf5f1e6");
+        url.append("&style=" +
+                "feature:road.arterial" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xfdfcf8");
+        url.append("&style=" +
+                "feature:road.highway" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xf8c967");
+        url.append("&style=" +
+                "feature:road.highway" + "%7C" +
+                "element:geometry.stroke" + "%7C" +
+                "color:0xe9bc62");
+        url.append("&style=" +
+                "feature:road.highway.controlled_access" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xe98d58");
+        url.append("&style=" +
+                "feature:road.highway.controlled_access" + "%7C" +
+                "element:geometry.stroke" + "%7C" +
+                "color:0xdb8555");
+        url.append("&style=" +
+                "feature:road.local" + "%7C" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0x806b63");
+        url.append("&style=" +
+                "feature:transit.line" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xdfd2ae");
+        url.append("&style=" +
+                "feature:transit.line" + "%7C" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0x8f7d77");
+        url.append("&style=" +
+                "feature:transit.line" + "%7C" +
+                "element:labels.text.stroke" + "%7C" +
+                "color:0xebe3cd");
+        url.append("&style=" +
+                "feature:transit.station" + "%7C" +
+                "element:geometry" + "%7C" +
+                "color:0xdfd2ae");
+        url.append("&style=" +
+                "feature:water" + "%7C" +
+                "element:geometry.fill" + "%7C" +
+                "color:0xb9d3c2");
+        url.append("&style=" +
+                "feature:water" + "%7C" +
+                "element:labels.text.fill" + "%7C" +
+                "color:0x92998d");
     }
 
     private static void setZoom() {
