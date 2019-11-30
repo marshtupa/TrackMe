a95ffd7c7240649b13f6512155aa60b0e9b8541b
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-11-30 10:43:19 +0300

    Create RegisterUser.java

---
+++ src/app/src/main/java/com/triple/trackme/Server/RegisterUser.java
@@ -0,0 +1,12 @@
+package com.triple.trackme.Server;
+
+import android.content.Context;
+
+import com.triple.trackme.R;
+
+public class RegisterUser {
+
+    public RegisterUser(Context context) {
+        context.getResources().getValue(R.id.);
+    }
+}
