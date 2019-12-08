631288c86d46cdddde2919dc3f8f41d3db638356
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-12-08 14:15:54 +0300

    Update sign up activity

--- src/app/src/main/java/com/triple/trackme/Activity/SignUp/SignUpActivity.java
+++ src/app/src/main/java/com/triple/trackme/Activity/SignUp/SignUpActivity.java
@@ -8,16 +8,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.triple.trackme.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private boolean firstNameValid = false;
+    private boolean secondNameValid = false;
+    private boolean emailValid = false;
+    private boolean passwordValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         updateWindow();
         setContentView(R.layout.activity_sign_up);
+        setFieldsValidation();
     }
 
     private void updateWindow() {
@@ -36,5 +44,54 @@ public class SignUpActivity extends AppCompatActivity {
             winParams.flags &= ~bits;
         }
         win.setAttributes(winParams);
+    }
+
+    private void setFieldsValidation() {
+        updateUI();
+
+        EditText firstName = findViewById(R.id.firstNameText);
+        firstName.addTextChangedListener(new TextValidator(firstName) {
+            @Override public void validate(TextView textView, String text) {
+                firstNameValid = !text.isEmpty();
+                updateUI();
+            }
+        });
+
+        final EditText secondName = findViewById(R.id.secondNameText);
+        secondName.addTextChangedListener(new TextValidator(secondName) {
+            @Override public void validate(TextView textView, String text) {
+                secondNameValid = !text.isEmpty();
+                updateUI();
+            }
+        });
+
+        EditText email = findViewById(R.id.emailText);
+        email.addTextChangedListener(new TextValidator(email) {
+            @Override public void validate(TextView textView, String text) {
+                String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+                emailValid = text.matches(regex);
+                updateUI();
+            }
+        });
+
+        EditText password = findViewById(R.id.passwordText);
+        password.addTextChangedListener(new TextValidator(password) {
+            @Override public void validate(TextView textView, String text) {
+                passwordValid = !text.isEmpty() && text.length() >= 6;
+                updateUI();
+            }
+        });
+    }
+
+    private void updateUI() {
+        if (firstNameValid && secondNameValid && emailValid && passwordValid) {
+            findViewById(R.id.signUpButton).setEnabled(true);
+        } else {
+            findViewById(R.id.signUpButton).setEnabled(false);
+        }
+    }
+
+    public void signUp(final View view) {
+
     }
 }
